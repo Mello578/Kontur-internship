80551f99ff03d3563db46c3a52442998f997006e
mello <mello.shlyapa@gmail.com>
2018-02-22 00:55:58 +0500

    +

--- src/client/js/gamePoints.js
+++
@@ -1,4 +0,0 @@
-export function gamePoints(countRemainingOrOpenCard) {
-	let pointBoard = document.getElementById('points');
-	pointBoard.innerText = parseInt(pointBoard.innerText) + countRemainingOrOpenCard * 42;
-}
